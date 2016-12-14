964aea3754fc5398b5e679bbc9367a31195e1963
Peter Springer <peterspringer829@gmail.com>
2016-12-14 15:24:04 -0700

    npm install redux

--- app/components/app.js
+++ app/components/app.js
@@ -11,8 +11,8 @@ let credentials = require('../../credentials');
 let lock = new Auth0Lock(credentials);
 
 export default class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       userToken: 'token',
     };
