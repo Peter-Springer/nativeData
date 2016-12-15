b367fc75c2215ef7e63fa44e74de571b023ed222
Adam Bernard <ab255@me.com>
2016-12-14 20:57:32 -0700

    AsyncStorage set up to save session token

--- app/components/app.js
+++ app/components/app.js
@@ -1,40 +1,78 @@
 import React, { Component } from 'react';
 import {
+  AsyncStorage,
   StyleSheet,
   Text,
+  TouchableHighlight,
   View,
-  TouchableHighlight
 } from 'react-native';
-
 import Auth0Lock from 'react-native-lock';
-let credentials = require('../../credentials');
-let lock = new Auth0Lock(credentials);
+
+import credentials from '../../credentials';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userToken: 'token',
+      userToken: null,
+      profile: null,
+      uid: 'uid904837920',
+
     };
   }
 
+  saveToken = async (token) => {
+    try {
+      await AsyncStorage.setItem(uid, token)
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  }
+
+  deleteToken = async () => {
+    try {
+      await AsyncStorage.removeItem(uid)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   login() {
+    let lock = new Auth0Lock(credentials);
     lock.show({}, (err, profile, token) => {
       if (err) {
         console.log(err);
         return;
       }
-      this.setState({ userToken: token });
-      console.log(this.state.userToken);
-      console.log(profile);
+      let sessionToken = JSON.stringify(token.idToken)
+      this.saveToken(sessionToken);
+      this.setState({ userToken: token, profile: profile });
     });
   }
 
-  logout() {
-    console.log('logout');
+  logout = async () => {
+    try {
+      await this.deleteToken()
+      this.setState({ userToken: null, profile: null, uid: Date.now() })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getItem = async () => {
+    try {
+      await AsyncStorage.getItem(uid, (err, result) => {
+        console.log(result);
+      })
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   render() {
+    console.log(this.state.userToken)
+    console.log(this.state.profile)
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -45,6 +83,9 @@ export default class App extends Component {
         </TouchableHighlight>
         <TouchableHighlight onPress={() => this.logout()}>
           <Text>Logout</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={() => this.getItem()}>
+          <Text>AsyncStorage</Text>
         </TouchableHighlight>
       </View>
     );
